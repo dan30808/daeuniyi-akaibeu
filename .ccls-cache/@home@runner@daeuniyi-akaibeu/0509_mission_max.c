67eb308b34937f8f53df732de39a9a81271ea5fb
dan30808 <129129693+dan30808@users.noreply.github.com>
2023-05-09 01:10:09 +0000

    mission_max

---
+++ .ccls-cache/@home@runner@daeuniyi-akaibeu/0509_mission_max.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+main(){
+  int a, b;  
+  printf("서로 다른 두 개의 정수를 입력\n");
+
+  printf("첫 번째 정수 입력");
+  scanf("%d" , &a);
+
+  printf("두 번째 정수 입력");
+   scanf("%d" , &b);
+  
+  printf("첫 번째 입력된 정수는 %d이고 두 번째 입력된 정수는 %d이며 두 정수 중 큰 수는 %d입니다.\n", a, b, max(a,b));
+}
+
+int max(int a, int b)
+{
+  if(a > b)
+    return a;
+  else
+    return b;
+}    
